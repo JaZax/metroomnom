030b872ff9212bae10c2b3bee3355de0a31ad47c
JaZa <47424360+JaZax@users.noreply.github.com>
2020-10-29 18:04:22 +0100

    + input box changing tempo on input + more styling

--- src/app.js
+++ src/app.js
@@ -9,16 +9,15 @@ const App = () => {
     let [tempo, changeTempo] = useState(100)
 
     window.onload = () => {
+        document.getElementById('inputTempoBox').value = tempo
         document.getElementById('inputTempo').addEventListener('input', ()=>{
             changeTempo(inputTempo.value)
             document.getElementById('inputTempoBox').value = document.getElementById('inputTempo').value
         })
 
-        window.addEventListener('keydown', (e)=>{
-            if(e.key == 'Enter'){
-                document.getElementById('inputTempo').value = document.getElementById('inputTempoBox').value
-                changeTempo(inputTempo.value)
-            }
+        document.getElementById('inputTempoBox').addEventListener('input', ()=>{
+            document.getElementById('inputTempo').value = document.getElementById('inputTempoBox').value
+            changeTempo(inputTempo.value)
         })
     }
 
@@ -26,13 +25,13 @@ const App = () => {
 
     return (
         <>
-            
+        
             <div id="left">
-                <input type="number" id="inputTempoBox"/>
+                <input type="number" id="inputTempoBox" min="1" max="500"/>
             </div>
 
             <div id="right">
-                <input type="range" id="inputTempo" min="1" max="400"/>
+                <input type="range" id="inputTempo" min="1" max="500"/>
             </div>
 
             <Metronome tempoVal={tempo}></Metronome>
